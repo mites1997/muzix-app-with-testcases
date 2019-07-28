962aed852de48e4210157b062765e86c2606fcbc
mites1997 <51950018+mites1997@users.noreply.github.com>
2019-07-28 21:26:29 +0530

    Update Track.java

--- src/main/java/com/stackroute/domain/Track.java
+++ src/main/java/com/stackroute/domain/Track.java
@@ -18,7 +18,6 @@ import javax.persistence.Id;
 public class Track {
 
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
     int id;
     String name;
     String comment;
@@ -28,35 +27,6 @@ public class Track {
 
 
 
-   /* public Track(int id, String name, String comment) {
-        this.id = id;
-        this.name = name;
-        this.comment = comment;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-*/
+  
 
 }
